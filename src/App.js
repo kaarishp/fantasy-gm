4f0daf2cdaa967ac58323d42d45411eb6f6d8e70
kaarishp <kingkaarish@gmail.com>
2024-03-25 23:21:18 -0400

    axios added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
-function App() {
+class App extends Component() {
+  constructor(props){
+    super(props);
+    this.state = {
+      playerName: null,
+      playerStats: {}
+    };
+  }
+  render(){
   return (
     <div className="App">
       
     </div>
   );
+}
 }
 
 export default App;
